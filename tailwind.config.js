fa03f5dab7e921222369a9ca94705954312df1e4
Suraj Joshi <darkkmatter20@gmail.com>
2024-12-08 21:02:40 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        system: {
+          purple50: '#f5f3ff', // Example custom color
+          green60: '#10b981',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
